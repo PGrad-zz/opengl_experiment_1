8c3a6273d933a108759125aa462d340d1823f3b3
PGrad <paulgrad3rd@gmail.com>
2018-08-26 21:27:40 -0700

    Shaders were wrong yo

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -1,7 +1,11 @@
 #version 450 core
 uniform sampler2D texture_diffuse;
-in vec2 texCoords;
+in GS_OUT {
+    vec3 position;
+    vec2 texCoords;
+    vec3 normal;
+} gs_out;
 out vec4 FragColor;
 void main() {
-	FragColor = texture(texture_diffuse, texCoords);
+	FragColor = texture(texture_diffuse, gs_out.texCoords);
 }
--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -7,10 +7,16 @@ uniform mat4 model;
 uniform mat4 view;
 uniform mat4 proj;
 
-out vec2 texCoords;
+out VS_OUT {
+    vec3 position;
+    vec2 texCoords;
+    vec3 normal;
+} vs_out;
 
 void main()
 {
-    texCoords = aTexCoords;
-    gl_Position = proj * view * vec4(aPos, 1.);
+    vs_out.position = (view * vec4(aPos, 1.)).xyz;
+    vs_out.texCoords = aTexCoords;
+    vs_out.normal = (vec4(aNormal, 1.) * view).xyz;
+    gl_Position = proj * vec4(vs_out.position, 1.);
 }
